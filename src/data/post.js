d8b5222d664da4e64565188ac019185998a1541e
Boato Andrea <163646394+aandrea-boatoo@users.noreply.github.com>
2024-12-12 17:03:21 +0100

    scaffolding

---
+++ src/data/post.js
@@ -0,0 +1,39 @@
+const dessert = [
+    {
+        id: 1,
+        title: "Ciambellone",
+        content: "Ricetta orginale del ciambellone",
+        img: "<img src='./images/ciambellone.jpeg'>",
+        tags: ["", "", "", "", ""],
+    },
+    {
+        id: 2,
+        title: "Cracker alla Barbabietola",
+        content: "ricetta originale del cracke r alla barbabietola",
+        img: "<img src='./images/cracker_barbabietola'>",
+        tags: ["", "", "", "", ""],
+    },
+    {
+        id: 3,
+        title: "Pane fritto dolce",
+        content: "Ricetta originale del pane dolce fritto",
+        img: "<img src='./images/pane_fritto_dolce.jpg'>",
+        tags: ["", "", "", "", ""],
+    },
+    {
+        id: 4,
+        title: "Pasta alla Barbabietola",
+        content: "Ricetta originale della Pasta alla Barbabietola",
+        img: "<img src='./images/pasta_barbabietola.jpg'>",
+        tags: ["", "", "", "", ""],
+    },
+    {
+        id: 5,
+        title: "Torta paesana",
+        content: "Ricetta originale della torta paesana",
+        img: "<img src='./images/torta_paesana.jpg'>",
+        tags: ["", "", "", "", ""],
+    },
+]
+
+module.exports = dessert;
